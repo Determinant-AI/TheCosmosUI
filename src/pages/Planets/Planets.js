6b541a0165b902770d31cd5c4680ace6b7491c42
fp2302 <fan@bricks-cloud.com>
2023-09-23 21:04:12 -0700

    be concise

--- src/pages/Planets/Planets.js
+++ src/pages/Planets/Planets.js
@@ -3,14 +3,14 @@ import { planets } from "../../constants";
 import PageHeader from "../../components/PageHeader/PageHeader";
 import CelestialObjectCardsContainer from "../../components/CelestialObjectCardsContainer/CelestialObjectCardsContainer";
 import CelestialObjectCard from "../../components/CelestialObjectCard/CelestialObjectCard";
-import { StyledSection, StyledP } from "./StyledPlanets";
+import { StyledSection } from "./StyledPlanets";
 
 const Planets = () => (
   <>
     <PageHeader title="Team"/>
-    <StyledP>
-    Our founding team comprises experts with a wealth of experience from the most popular game platforms: Unity and Roblox. We've honed our skills in building player engagement models, training AI agents through simulation, and providing innovative monetization solutions. In this era of AI, we're leveraging our expertise to shape the future of gaming.
-    </StyledP>
+    <p>
+      Our founding team comprises experts with a wealth of experience from the most popular game platforms: Unity and Roblox. We've honed our skills in building player engagement models, training AI agents through simulation, and providing innovative monetization solutions. In this era of AI, we're leveraging our expertise to shape the future of gaming.
+    </p>
     <StyledSection>
       <CelestialObjectCardsContainer>
         {planets.map((planet) => (
